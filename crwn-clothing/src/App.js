f1aed42010979d6121eee9292073ed2be9c500e7
An Phuc Thien Nguyen <npta97@gmail.com>
2020-08-17 16:10:28 +0700

    Add ShopPage, CollectionPreview, CollectionItem components

--- crwn-clothing/src/App.js
+++ crwn-clothing/src/App.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 
-import HomePage from './pages/homepage/homepage.component';
+import HomePage from './pages/home-page/home-page.component';
+import ShopPage from './pages/shop-page/shop-page.component';
 
 import './App.css';
 
@@ -10,6 +11,7 @@ function App() {
     <div>
       <Switch>
         <Route exact path='/' component={HomePage} />
+        <Route path='/shop' component={ShopPage} />
       </Switch>
     </div>
   );
