4b9c82deff8c4016122c934b6fbcb77a0a88304a
An Phuc Thien Nguyen <npta97@gmail.com>
2020-08-17 14:17:02 +0700

    Add React Router

--- crwn-clothing/src/App.js
+++ crwn-clothing/src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
 
 import HomePage from './pages/homepage/homepage.component';
 
@@ -6,8 +7,10 @@ import './App.css';
 
 function App() {
   return (
-    <div className='App'>
-      <HomePage />
+    <div>
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+      </Switch>
     </div>
   );
 }
