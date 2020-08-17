6875b4f2470fdbb8b82d92ca22d56fac3bc0e522
An Phuc Thien Nguyen <npta97@gmail.com>
2020-08-17 17:37:33 +0700

    Add SignIn component

--- crwn-clothing/src/App.js
+++ crwn-clothing/src/App.js
@@ -4,6 +4,7 @@ import { Route, Switch } from 'react-router-dom';
 import Header from './components/header/header.component';
 import HomePage from './pages/home-page/home-page.component';
 import ShopPage from './pages/shop-page/shop-page.component';
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up-page/sign-in-and-sign-up-page.component';
 
 import './App.css';
 
@@ -13,6 +14,7 @@ function App() {
       <Header />
       <Switch>
         <Route exact path='/' component={HomePage} />
+        <Route path='/signin' component={SignInAndSignUpPage} />
         <Route path='/shop' component={ShopPage} />
       </Switch>
     </div>
