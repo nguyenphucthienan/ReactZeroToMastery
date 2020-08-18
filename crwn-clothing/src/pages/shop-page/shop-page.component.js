43b66be7b73e17e9a8693a5cc5d4d6629c084b38
An Phuc Thien Nguyen <npta97@gmail.com>
2020-08-18 22:33:47 +0700

    Refactor to use Redux

--- crwn-clothing/src/pages/shop-page/shop-page.component.js
+++ crwn-clothing/src/pages/shop-page/shop-page.component.js
@@ -1,30 +1,13 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-import CollectionPreview from '../../components/collection-preview/collection-preview.component';
-
-import SHOP_DATA from './shops.data';
+import CollectionsOverview from '../../components/collections-overview/collections-overview.component';
 
 import './shop-page.styles.scss';
 
-class ShopPage extends Component {
-  constructor() {
-    super();
-
-    this.state = {
-      collections: SHOP_DATA
-    };
-  }
-
-  render() {
-    const { collections } = this.state;
-    return (
-      <div className='shop-page'>
-        {collections.map(({ id, ...otherCollectionProps }) => (
-          <CollectionPreview key={id} {...otherCollectionProps} />
-        ))}
-      </div>
-    );
-  }
-}
+const ShopPage = () => (
+  <div className='shop-page'>
+    <CollectionsOverview />
+  </div>
+);
 
 export default ShopPage;
