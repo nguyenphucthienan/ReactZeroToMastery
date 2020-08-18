ce00a6b54b967f73cc66a1dc5885a6757cb2e473
An Phuc Thien Nguyen <npta97@gmail.com>
2020-08-18 16:13:08 +0700

    Add Reselect

--- crwn-clothing/src/components/cart-icon/cart-icon.component.js
+++ crwn-clothing/src/components/cart-icon/cart-icon.component.js
@@ -5,17 +5,23 @@ import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg';
 
 import { toggleCartHidden } from '../../redux/cart/cart.actions';
 
+import { selectCardItemsCount } from '../../redux/cart/cart.selectors';
+
 import './cart-icon.styles.scss';
 
-const CartIcon = ({ toggleCartHidden }) => (
+const CartIcon = ({ itemCount, toggleCartHidden }) => (
   <div className='cart-icon' onClick={toggleCartHidden}>
     <ShoppingIcon className='shopping-icon' />
-    <span className='item-count'>0</span>
+    <span className='item-count'>{itemCount}</span>
   </div>
 );
+
+const mapStateToProps = state => ({
+  itemCount: selectCardItemsCount(state)
+});
 
 const mapDispatchToProps = dispatch => ({
   toggleCartHidden: () => dispatch(toggleCartHidden())
 });
 
-export default connect(null, mapDispatchToProps)(CartIcon);
+export default connect(mapStateToProps, mapDispatchToProps)(CartIcon);
