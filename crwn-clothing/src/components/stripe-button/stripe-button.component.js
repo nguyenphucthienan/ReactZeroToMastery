ce0a272bed8ffd6df716effcd5c8cef89ac3f063
An Phuc Thien Nguyen <npta97@gmail.com>
2020-08-19 09:37:14 +0700

    Add Stripe payment

---
+++ crwn-clothing/src/components/stripe-button/stripe-button.component.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+
+import './stripe-button.styles.scss';
+
+const StripeCheckoutButton = ({ price }) => {
+  const priceForStripe = price * 100;
+  const publicshableKey = 'pk_test_lqljVsejrn3xlFRpbqC31g8I';
+
+  const onToken = token => {
+    console.log(token);
+    alert('Payment Successful');
+  };
+
+  return (
+    <StripeCheckout
+      label='Pay Now'
+      panelLabel='Pay Now'
+      name='CRWN Clothing Ltd.'
+      billingAddress
+      shippingAddress
+      image='https://sendeyo.com/up/d/f3eb2117da'
+      description={`Your total is $${price}`}
+      amount={priceForStripe}
+      token={onToken}
+      stripeKey={publicshableKey}
+    />
+  );
+};
+
+export default StripeCheckoutButton;
