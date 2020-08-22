da41a49ef10a21de64c838c7746467adb7c0171a
An Phuc Thien Nguyen <npta97@gmail.com>
2020-08-22 16:44:13 +0700

    Integrate Stripe with backend

--- crwn-clothing/src/components/stripe-button/stripe-button.component.js
+++ crwn-clothing/src/components/stripe-button/stripe-button.component.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import axios from 'axios';
 import StripeCheckout from 'react-stripe-checkout';
 
 import './stripe-button.styles.scss';
@@ -8,8 +9,23 @@ const StripeCheckoutButton = ({ price }) => {
   const publicshableKey = 'pk_test_lqljVsejrn3xlFRpbqC31g8I';
 
   const onToken = token => {
-    console.log(token);
-    alert('Payment Successful');
+    axios({
+      url: 'payment',
+      method: 'post',
+      data: {
+        amount: priceForStripe,
+        token
+      }
+    })
+      .then(response => {
+        alert('Payment successful.');
+      })
+      .catch(error => {
+        console.log('Payment error', error);
+        alert(
+          'There was an issue with your payment. Please sure you use the provided credit card.'
+        );
+      });
   };
 
   return (
